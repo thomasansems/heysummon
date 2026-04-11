74b8f3b4bef2c768fe04c84cedff8b0f72a090af
Thomas Ansems <thomasansems@gmail.com>
2026-04-11 10:31:07 +0200

    fix: remove VideoSection from App component and update messaging in changelog to emphasize platform-agnostic capabilities

--- landingspage/src/App.tsx
+++ landingspage/src/App.tsx
@@ -22,7 +22,6 @@ export default function App() {
         <Navbar />
         <main>
           <Hero />
-          <VideoSection />
           <ExpertsSection />
           <ClientsSection />
           <FlowSection />
